bea01a1d4db1852aabc53adec8534f410bba7e26
Clinton Oyedeji <>
2021-05-18 15:32:35 +0000

    missing character,string and boolean

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,16 @@
 public class Main {
     public static void main(String[] args){
     int myIntger=13;
+    double num=6.7;
+    float num1=48f;
+    float num2=36f;
+    
+    System.out.println(char1+char2);
+    System.out.println(num1+num2);
+    System.out.println(myIntger+num);
     System.out.println(myIntger);
   
     }
     
 }
+
